5303e982306a873869c8ffec78aa10bcf907c5c9
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2022-04-22 23:24:40 +0100

    wip: made small refactor and implemented the file elimination

--- src/main.js
+++ src/main.js
@@ -1,29 +1,6 @@
 import express from "express";
-import multer from "multer";
-import multerS3 from "multer-s3";
-import AWS from "aws-sdk";
 
-const s3 = new AWS.S3({
-  endpoint: "http://127.0.0.1:9000",
-  accessKeyId: "ly1y6iMtYf",
-  secretAccessKey: "VNcmMuDARGGstqzkXF1Van1Mlki5HGU9",
-  sslEnabled: false,
-  s3ForcePathStyle: true,
-});
-
-var upload = multer({
-  storage: multerS3({
-    s3,
-    bucket: "dev-multer-s3-bucket",
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    metadata: (req, file, cb) => {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: (req, file, cb) => {
-      cb(null, Date.now().toString());
-    },
-  }),
-});
+import { s3, bucket, upload } from "./middleware/bucket.js";
 
 const startServer = async () => {
   const app = express();
@@ -36,6 +13,28 @@ const startServer = async () => {
 
   app.post("/upload", upload.single("file"), (req, res) => {
     return res.json({ message: req.file.location });
+  });
+
+  app.delete("/remove/:key", async (req, res) => {
+    const params = { Bucket: bucket, Key: req.params.key };
+
+    let file
+
+    try {
+      file = await s3.headObject(params).promise();
+    } catch (error) {
+      return res.status(404).json({ message: "File not found" });
+    }
+
+    if (file) {
+      try {
+        await s3.deleteObject(params).promise();
+      } catch (error) {
+        return res.status(500).json({ message: "Could not delete file" });
+      }
+    }
+
+    return res.json({ message: "File deleted" });
   });
 
   return app;
